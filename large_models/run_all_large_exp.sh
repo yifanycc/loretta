39e27f46bd0278fb4612706972902a7f23bf53c0
Yifan Yang <yifanyang@ucsb.edu>
2024-04-21 16:32:18 -0700

    Update run_all_large_exp.sh

--- large_models/run_all_large_exp.sh
+++ large_models/run_all_large_exp.sh
@@ -7,9 +7,9 @@ export WANDB_START_METHOD="thread"
 # loretta_adp, loretta_rep, lora, adapters (series), prompt, ia3, ptune
 
 # test examples (for SST2 task)
-MODEL=meta-llama/Llama-2-7b-hf TASK=SST2 MODE=loretta_adp EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
-MODEL=meta-llama/Llama-2-7b-hf TASK=SST2 MODE=loretta_rep EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
-MODEL=meta-llama/Llama-2-7b-hf TASK=SST2 MODE=lora EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
-MODEL=meta-llama/Llama-2-7b-hf TASK=SST2 MODE=adapters EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
-MODEL=meta-llama/Llama-2-7b-hf TASK=SST2 MODE=prompt EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
-MODEL=meta-llama/Llama-2-7b-hf TASK=SST2 MODE=ia3 EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
+MODEL=meta-llama/Llama-2-7b-hf TASK=BoolQ MODE=loretta_adp EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
+MODEL=meta-llama/Llama-2-7b-hf TASK=BoolQ MODE=loretta_rep EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
+MODEL=meta-llama/Llama-2-7b-hf TASK=BoolQ MODE=lora EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
+MODEL=meta-llama/Llama-2-7b-hf TASK=BoolQ MODE=adapters EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
+MODEL=meta-llama/Llama-2-7b-hf TASK=BoolQ MODE=prompt EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
+MODEL=meta-llama/Llama-2-7b-hf TASK=BoolQ MODE=ia3 EPOCH=3 BS=8 LR=1e-4 DEVICE=7 bash finetune.sh
